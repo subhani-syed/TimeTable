e787653c69de4f2bcf5f84ad22f7100fbd9ca2e0
subhani-syed <subhani6352@gmail.com>
2021-12-24 15:55:27 +0530

    Added Routing

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,'/index.html'));
 });
 
+// Routing For Weekdays
+app.get("/monday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/monday.html'));
+});
+app.get("/tuesday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/tuesday.html'));
+});
+app.get("/wednesday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/wednesday.html'));
+});
+app.get("/thursday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/thursday.html'));
+});
+app.get("/friday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/friday.html'));
+});
+app.get("/saturday",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/saturday.html'));
+});
+
+
 app.listen(PORT,()=>{
-    console.log("App is running at port ${PORT}")
+    console.log("App is running at port ${PORT}");
 });
